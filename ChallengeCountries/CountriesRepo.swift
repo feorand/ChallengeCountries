ce26bda52f9fee7d931fbbdd3f07dc350a94d8b3
Pavel Prokofyev <feorand@gmail.com>
2018-07-30 18:06:41 +0500

    implement server response parse (finish)

--- ChallengeCountries/CountriesRepo.swift
+++ ChallengeCountries/CountriesRepo.swift
@@ -32,13 +32,14 @@ class CountriesRepo {
                     self.next = parsedResult.next
                     self.countries = parsedResult.countries
                 } catch {
-                    
+                    print(error)
                 }
                 
             case .failure(let error):
                 print(error)
             }
         }
+        
         handler()
     }
     
@@ -63,21 +64,38 @@ class CountriesRepo {
             return result
         }
         
-        func extractEntry<Type>(from dictionary: JSONDictionary,key: String, nodeName: String, index: Int = 0) throws -> Type {
-            guard let result = dictionary[key] as? Type else { throw getError(name: nodeName, index: index) }
+        func extractEntry<Type>(from dictionary: JSONDictionary,key: String, index: Int = 0) throws -> Type {
+            guard let result = dictionary[key] as? Type else { throw getError(name: key, index: index) }
             return result
         }
         
-        let initialDictionary: JSONDictionary = try extract(from: rawData, nodeName: "Main")
-        
-        let next:String = try extractEntry(from: initialDictionary, key: "next", nodeName: "Next")
-        
-        let countries:JSONArray = try extract(from: initialDictionary, nodeName: "Countries")
-        
-        for (index, countryRaw) in countries.enumerated() {
+        func parseCountry(countryRaw: Any, index: Int) throws -> Country{
             let country: JSONDictionary = try extract(from: countryRaw, nodeName: "country", index: index)
+            
+            let name: String = try extractEntry(from: country, key: "name", index: index)
+            let capital: String = try extractEntry(from: country, key: "capital", index: index)
+            let continent: String = try extractEntry(from: country, key: "continent", index: index)
+            let population: Int = try extractEntry(from: country, key: "population", index: index)
+            let descriptionSmall: String = try extractEntry(from: country, key: "description_small",index: index)
+            let description: String = try extractEntry(from: country, key: "description", index: index)
+            let imageUrl: String = try extractEntry(from: country, key: "image", index: index)
+            
+            let countryInfo: JSONDictionary = try extractEntry(from: country, key: "country_info", index: index)
+            let imagesUrls: [String] = try extractEntry(from: countryInfo, key: "images", index: index)
+            let flagUrl: String = try extractEntry(from: countryInfo, key: "flag", index: index)
+            
+            let resultImagesUrls = imageUrl.isEmpty ? imagesUrls : [imageUrl]
+            return Country(name: name, continent: continent, capital: capital, population: population, descriptionSmall: descriptionSmall, description: description, flagUrl: flagUrl, photosUrls: resultImagesUrls)
         }
         
-        return (next, [])
+        let initialDictionary: JSONDictionary = try extract(from: rawData, nodeName: "main")
+        
+        let next:String = try extractEntry(from: initialDictionary, key: "next")
+        
+        let countriesRaw:JSONArray = try extractEntry(from: initialDictionary, key: "countries")
+        
+        let countries = try countriesRaw.enumerated().map { try parseCountry(countryRaw: $0.element, index: $0.offset) }
+        
+        return (next,countries)
     }
 }
