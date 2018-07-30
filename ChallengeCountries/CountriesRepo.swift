356c1eb03edfdcb667bc97135d5cb9be52bd0a6a
Pavel Prokofyev <feorand@gmail.com>
2018-07-30 15:59:41 +0500

    add request to server

--- ChallengeCountries/CountriesRepo.swift
+++ ChallengeCountries/CountriesRepo.swift
@@ -7,10 +7,12 @@
 //
 
 import Foundation
+import Alamofire
 
 struct RepoConstants {
     static let InitialUrl = "https://rawgit.com/NikitaAsabin/799e4502c9fc3e0ea7af439b2dfd88fa/raw/7f5c6c66358501f72fada21e04d75f64474a7888/page1.json"
 }
+
 
 class CountriesRepo {
     private var next: String = ""
@@ -18,8 +20,16 @@ class CountriesRepo {
     var countries: [Country] = []
     
     func updateCountries(handler: ()->Void) {
-        //TODO: Actual update here
-        
+        request(RepoConstants.InitialUrl).responseJSON{ response in
+            switch response.result {
+            case .success(let value):
+                let parsedResult = self.parseJSONResult(rawData: value)!
+                self.next = parsedResult.next
+                self.countries = parsedResult.countries
+            case .failure(let error):
+                print(error)
+            }
+        }
         handler()
     }
     
@@ -27,5 +37,11 @@ class CountriesRepo {
         //TODO: Actual get here
         
         handler()
+    }
+    
+    private func parseJSONResult(rawData: Any) -> (next: String, countries: [Country])? {
+        //guard let initialDictionary = rawData as? [String: Any] else { return nil }
+        
+        return nil
     }
 }
--- ChallengeCountries/ViewController.swift
+++ ChallengeCountries/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let countriesRepo = CountriesRepo()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        countriesRepo.updateCountries(handler: {  })
     }
 
     override func didReceiveMemoryWarning() {
