57e21246e688186b12dda30a5aeff5f50e0fa172
Pavel Prokofyev <feorand@gmail.com>
2018-08-01 16:40:09 +0500

    add details scene

---
+++ ChallengeCountries/CountryDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CountryDetailsViewController.swift
+//  ChallengeCountries
+//
+//  Created by Pavel Prokofyev on 01.08.2018.
+//  Copyright © 2018 Pavel Prokofyev. All rights reserved.
+//
+
+import UIKit
+
+class CountryDetailsViewController: UIViewController {
+
+}
