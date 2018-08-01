2de788081fa536701e6cbfd15e042a60841741c5
Pavel Prokofyev <feorand@gmail.com>
2018-08-01 14:24:26 +0500

    implement custom cell display

--- ChallengeCountries/CountriesListViewController.swift
+++ ChallengeCountries/CountriesListViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+struct CountriesListConstants {
+    static let FlagImageAspectRatio: CGFloat = 50.0 / 34.0
+    static let FlagToViewWidthAspectRatio: CGFloat = 50.0 / 320.0
+    
+    static let FlagTopSpacing: CGFloat = 16.0
+    static let FlagDescriptionSpacing: CGFloat = 11.0
+    static let DescriptionBottomSpacing: CGFloat = 16.0
+}
+
 class CountriesListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+        
     let countriesRepo = CountriesRepo()
 
     override func viewDidLoad() {
@@ -35,17 +44,52 @@ extension CountriesListViewController: UITableViewDelegate, UITableViewDataSourc
         return countriesRepo.countries.count
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard countriesRepo.countries.count > 0 else { return 0 }
+        
+        let country = countriesRepo.countries[indexPath.row]
+        let countryDescription = country.descriptionSmall as NSString
+        
+        let descriptionStringAttributes = [NSAttributedStringKey.font : UIFont.systemFont(ofSize: 15.0)]
+        let descriptionStringSize = CGSize(width: view.bounds.width, height: CGFloat.infinity)
+        let descriptionBoundingRect = countryDescription
+            .boundingRect(with: descriptionStringSize,
+                          options: .usesLineFragmentOrigin,
+                          attributes: descriptionStringAttributes,
+                          context: nil)
+        let descriptionHeight = descriptionBoundingRect.height
+        
+        let flagHeight = view.bounds.width *
+            CountriesListConstants.FlagToViewWidthAspectRatio /
+            CountriesListConstants.FlagImageAspectRatio
+        
+        let cellHeight = CountriesListConstants.FlagTopSpacing +
+            flagHeight +
+            CountriesListConstants.FlagDescriptionSpacing +
+            descriptionHeight +
+            CountriesListConstants.DescriptionBottomSpacing
+        
+        return cellHeight
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if countriesRepo.countries.count != 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
-            let country = countriesRepo.countries[indexPath.row]
-            cell.textLabel?.text = country.name
-            cell.detailTextLabel?.text = country.descriptionSmall
-            cell.imageView?.image = UIImage(data: country.flag!)
-            return cell
+        guard countriesRepo.countries.count > 0,
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell",
+                                                     for: indexPath) as? CountryCell else {
+            return UITableViewCell()
         }
         
-        return UITableViewCell()
+        let country = countriesRepo.countries[indexPath.row]
+        
+        if let flag = country.flag {
+            cell.flagImageView.image = UIImage(data: flag)
+        }
+        
+        cell.nameLabel.text = country.name
+        cell.capitalLabel.text = country.capital
+        cell.descriptionLabel.text = country.descriptionSmall
+        
+        return cell
     }
 }
 
