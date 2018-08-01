aa027f00f16bf8075b8b23c98f1197edd5b95c8e
Pavel Prokofyev <feorand@gmail.com>
2018-08-01 12:30:22 +0500

    add tableView delegate methods stubs and activity indicator

--- ChallengeCountries/CountriesListViewController.swift
+++ ChallengeCountries/CountriesListViewController.swift
@@ -8,21 +8,28 @@
 
 import UIKit
 
-class CountriesListViewController: UITableViewController {
+class CountriesListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     let countriesRepo = CountriesRepo()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        countriesRepo.updateCountries(completionHandler: {  })
+        
+        countriesRepo.updateCountries(completionHandler: {
+            
+        })
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension CountriesListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
